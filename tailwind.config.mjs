ac81bb48d60164499f3e0e51a2885adc1c669a6d
Manul Thanura <manulkarunarathna@gmail.com>
2024-04-10 11:20:10 +0530

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+	  screens: {
+		sm: "480px",
+		md: "768px",
+		lg: "976px",
+		xl: "1440px",
+	  },
+	  colors: {
+		black: "#000000",
+		white: "#FFFFFF",
+		gray: "#0D0C22",
+		stone: "#231F20",
+		lime: "#B9FF66",
+		zinc: {
+		  100: "#F3F3F3",
+		  200: "#F0F0F0",
+		  300: "#D9D9D9",
+		  400: "#D8D8D8",
+		  500: "#898989",
+		  600: "#B0B0B0",
+		  700: "#787878",
+		  800: "#292A32",
+		  900: "#191A23",
+		},
+	  },
+	  extend: {
+		fontFamily: {
+			SpaceGrotesk: ["Space Grotesk", "sans-serif"],
+		},
+	  },
 	},
-	plugins: [],
-}
+  };
