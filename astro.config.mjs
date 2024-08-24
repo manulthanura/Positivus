b12d38968fd1769a7c1a5decff5479b62e0b05e8
Manul Thanura <manulkarunarathna@gmail.com>
2024-08-24 10:24:09 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server'
 });
